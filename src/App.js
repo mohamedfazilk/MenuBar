f3f42154df5a3e7e9068066ce477c5bad95923a0
mohamedfazilk <2114921@cognizant.com>
2022-07-14 11:55:53 +0530

    list the menu items

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Menu from './Menu'
+import Categories from "./Categories";
+import items from './data'
 
 function App() {
+  const [menuitems, setMenuItems] = useState(items);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className='menu section'>
+      <div className="title">
+        <h2>Our Menu</h2>
+        <div className="underline"></div>
+      </div>
+      <Menu menuitems={menuitems}/>
+      </section>
+   
+    </main>
   );
 }
 
